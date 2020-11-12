91f0f1755f213c468c4bb086447cd63472316689
MVK-USA <mvk2015@yahoo.com>
2020-11-12 11:48:54 -0500

    new class

--- src/classHour1.java
+++ src/classHour1.java
@@ -1,6 +1,8 @@
 public class classHour1 {
     public static void main(String[] args) {
         System.out.println("Hello new class");
+        // Jelly bean missed
+        //Cocomelon
 
     }
 }
